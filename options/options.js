243b065877e23dd9cfc3fdca1137b7054ac98c4e
Daniel Sousa <daniel.s@crowdbotics.com>
2023-10-18 14:52:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-hall-2",
+  url: "https://blue-hall-2.botics.co",
+  api: "https://blue-hall-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
